7579ad31d0ce0fefeec3442304d510e7586c58ca
Hugues DUMONT <hu.du@free.fr>
2020-06-27 18:31:07 +0200

    Update test.vb

--- test.vb
+++ test.vb
@@ -1,2 +1,13 @@
-#This is a test file
-' And here is a new line
+Public num1 As Integer
+Public num2 As Integer
+Public answer As Integer
+
+Sub Main()
+    Console.WriteLine("Type a number and press Enter")
+    num1 = Console.ReadLine()
+    Console.WriteLine("Type another number to add to it and press Enter")
+    num2 = Console.ReadLine()
+    answer = num1 + num2
+    Console.WriteLine("The answer is " & answer)
+    Console.ReadLine()
+End Sub
